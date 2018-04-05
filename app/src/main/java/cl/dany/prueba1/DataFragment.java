9f3eff8ee240bca48a8e7f52d86a698426c5a3b9
Dany <davergarap@uc.cl>
2018-04-05 20:39:04 -0300

    Actualización de inputs

--- app/src/main/java/cl/dany/prueba1/DataFragment.java
+++ app/src/main/java/cl/dany/prueba1/DataFragment.java
@@ -2,12 +2,14 @@ package cl.dany.prueba1;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,6 +21,8 @@ import android.widget.Toast;
  */
 public class DataFragment extends Fragment {
 
+    int day ;
+    int month;
 
     public DataFragment() {
         // Required empty public constructor
@@ -35,11 +39,18 @@ public class DataFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-       final EditText txtDay = view.findViewById(R.id.txtDay);
-       final EditText txtMonth = view.findViewById(R.id.txtMonth);
+       final EditText txtName = view.findViewById(R.id.txtName);
+       final CalendarView txtCalendar = view.findViewById(R.id.txtCalendar);
        final ImageView image = view.findViewById(R.id.imgSign);
        final TextView txtData = view.findViewById(R.id.txtDataSign);
-
+   
+        txtCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int y, int m, int d) {
+                    day = d;
+                    month = m +1;
+            }
+        });
         Button btnFind = view.findViewById(R.id.btnFind);
          btnFind.setOnClickListener(new View.OnClickListener() {
              @Override
@@ -47,113 +58,113 @@ public class DataFragment extends Fragment {
 
 
 
-                 String day = txtDay.getText().toString();
+                 String name = txtName.getText().toString();
 
-                 String month = txtMonth.getText().toString();
+           //      String month = txtCalendar.getText().toString();
 
-                 if (day.trim().length() > 0 && month.trim().length() > 0)
+                 if (day > 0 && month  > 0)
                  {
-                     int dayBirth = Integer.parseInt(day);
-                     int monthBirth = Integer.parseInt(month);
+                   
 
-                     if(dayBirth >= 22  && dayBirth <= 31 && monthBirth == 12 || dayBirth <= 20  && dayBirth >= 1 && monthBirth == 1)
+                     if(day >= 22  && day <= 31 && month == 12 || day <= 20  && day >= 1 && month == 1)
                      {
+
                          image.setImageResource(R.drawable.capricornio);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("CAPRICORNIO \n\nEste signo está regido por Saturno, planeta que se relaciona con la responsabilidad, la cautela, la previsión, la búsqueda de la seguridad. \n" +
+                         txtData.setText(name + " tu signo es CAPRICORNIO \n\nEste signo está regido por Saturno, planeta que se relaciona con la responsabilidad, la cautela, la previsión, la búsqueda de la seguridad. \n" +
                                  "\n" +
                                  "Ambicioso y muy perseverante para lograr sus propósitos. Generalmente la primera mitad de la vida es un tanto tímido y la segunda mitad de la vida va ganando en confianza. ");
 
 
-                     }else if(dayBirth >= 21 && dayBirth <= 31 && monthBirth == 1 || dayBirth <= 19 && dayBirth >= 1 && monthBirth == 2)
+                     }else if(day >= 21 && day <= 31 && month == 1 || day <= 19 && day >= 1 && month == 2)
                      {
 
                          image.setImageResource(R.drawable.aquario);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("ACUARIO \n\nAcuario es un signo regido por Urano, es un signo de aire y de naturaleza fija. Son personas sociables y pueden entablar lazos estables con personas de diversos grupos sociales, étnicos, religiosos, etc. \n" +
+                         txtData.setText(name + " tu signo es ACUARIO \n\nAcuario es un signo regido por Urano, es un signo de aire y de naturaleza fija. Son personas sociables y pueden entablar lazos estables con personas de diversos grupos sociales, étnicos, religiosos, etc. \n" +
                                  "\n" +
                                  "Los nativos de este signo siempre están interesados por el bien común. Son creativos, bastante desapegados de las cosas materiales y buscan constantemente la libertad. ");
-                     }else if(dayBirth >= 20 && dayBirth <= 28 && monthBirth == 2 || dayBirth <= 20 && dayBirth >= 1 && monthBirth == 3 )
+                     }else if(day >= 20 && day <= 28 && month == 2 || day <= 20 && day >= 1 && month == 3 )
                      {
                          image.setImageResource(R.drawable.piscis);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("PISCIS \n\nPiscis es un signo regido por Neptuno, que es un planeta que tiene que ver con la imaginación, la fantasía y con el ensueño. Los piscianos son muy emotivos, es un signo de agua, generalmente los piscianos aman mucho el arte y la música y poseen una fuerte conexión con su trabajo. \n" +
+                         txtData.setText(name + " tu signo es PISCIS \n\nPiscis es un signo regido por Neptuno, que es un planeta que tiene que ver con la imaginación, la fantasía y con el ensueño. Los piscianos son muy emotivos, es un signo de agua, generalmente los piscianos aman mucho el arte y la música y poseen una fuerte conexión con su trabajo. \n" +
                                  "\n" +
                                  "A pesar de ser introvertidos, los hombres y mujeres de Piscis suelen tener muchos amigos. Tienen una alegría natural que contagia y una fuerte profundidad emocional. ");
-                     }else if(dayBirth >= 21 && dayBirth <= 31 && monthBirth == 3  || dayBirth <= 20 && dayBirth >= 1 && monthBirth == 4)
+                     }else if(day >= 21 && day <= 31 && month == 3  || day <= 20 && day >= 1 && month == 4)
                      {
                          image.setImageResource(R.drawable.aries);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("ARIES \n\nAries es el primer signo del zodíaco, los hombres y mujeres de este signo por lo general tienen las siguientes características: son transparentes y frontales, no les gusta guardarse nada, expresan con total claridad lo que piensan y lo que sienten. Poseen un temperamento muy fuere y es muy fácil para las personas que los rodean, conocer el estado anímico del ariano. \n" +
+                         txtData.setText(name + " tu signo es ARIES \n\nAries es el primer signo del zodíaco, los hombres y mujeres de este signo por lo general tienen las siguientes características: son transparentes y frontales, no les gusta guardarse nada, expresan con total claridad lo que piensan y lo que sienten. Poseen un temperamento muy fuere y es muy fácil para las personas que los rodean, conocer el estado anímico del ariano. \n" +
                                  "\n" +
                                  "Es un signo de fuego, lo que implica decisión, mucha energía e impulsividad. Las personas de Aries frecuentemente actúan sin pensar. ");
-                     }else if(dayBirth >= 21 && dayBirth <= 30 && monthBirth == 4 || dayBirth <= 21 && dayBirth >= 1  && monthBirth == 5)
+                     }else if(day >= 21 && day <= 30 && month == 4 || day <= 21 && day >= 1  && month == 5)
                      {
                          image.setImageResource(R.drawable.tauro);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("TAURO  \n\nTauro es un signo de tierra regido por Venus, por este motivo las personas de este signo poseen una naturaleza sensual, elegante y refinada. \n" +
+                         txtData.setText(name + " tu signo es TAURO  \n\nTauro es un signo de tierra regido por Venus, por este motivo las personas de este signo poseen una naturaleza sensual, elegante y refinada. \n" +
                                  "\n" +
                                  "Los hombres y mujeres de Tauro sienten una gran inclinación hacia el confort, los lujos y hacia una vida material holgada. Disfrutan de la buena gastronomía y del aroma de los buenos perfumes. Les gusta que los traten con sumo cuidado y respeto y anhelan vivir en ambientes a armónicos y placenteros. ");
-                     }else if(dayBirth >= 22 && dayBirth <= 31 && monthBirth == 5 || dayBirth <= 21 && dayBirth >= 1 && monthBirth == 6)
+                     }else if(day >= 22 && day <= 31 && month == 5 || day <= 21 && day >= 1 && month == 6)
                      {
                          image.setImageResource(R.drawable.geminis);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("GËMINIS \n\nGéminis es un signo de aire regido por Mercurio. Los nativos de este signo son extremadamente comunicativos, gesticulan cuando hablan y pueden abarcar varios temas de una sola vez. \n" +
+                         txtData.setText(name + " tu signo es GÉMINIS \n\nGéminis es un signo de aire regido por Mercurio. Los nativos de este signo son extremadamente comunicativos, gesticulan cuando hablan y pueden abarcar varios temas de una sola vez. \n" +
                                  "\n" +
                                  "Los hombres y mujeres de Géminis son curiosos e inquietos, les gusta estar bien informados y saber qué es lo que pasa a su alrededor. Son muy sociables y necesitan que su vida sea ágil y esté en constante movimiento. Poseen una gran capacidad para adaptarse a diversas situaciones. ");
-                     }else  if(dayBirth >= 22 && dayBirth <= 30 && monthBirth == 6 || dayBirth <= 22 && dayBirth >= 1  && monthBirth == 7)
+                     }else  if(day >= 22 && day <= 30 && month == 6 || day <= 22 && day >= 1  && month == 7)
                      {
                          image.setImageResource(R.drawable.cancer);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("CANCER \n\nCáncer es un signo de agua de naturaleza emocional, regido por la luna. La luna está relacionada con el inconsciente y por lo tanto los nativos de este signo suelen poseer una fuerte memoria emotiva. \n" +
+                         txtData.setText(name + " tu signo es CANCER \n\nCáncer es un signo de agua de naturaleza emocional, regido por la luna. La luna está relacionada con el inconsciente y por lo tanto los nativos de este signo suelen poseer una fuerte memoria emotiva. \n" +
                                  "\n" +
                                  "En el ámbito del amor, el hombre de Cáncer suele ser sensible y muy sobreprotector con su pareja. Es romántico e idealista y buscará siempre una relación que le de seguridad afectiva. ");
-                     }else if(dayBirth >= 23 && dayBirth <= 31 && monthBirth == 7 || dayBirth <= 22 && dayBirth >= 1  && monthBirth == 8)
+                     }else if(day >= 23 && day <= 31 && month == 7 || day <= 22 && day >= 1  && month == 8)
                      {
                          image.setImageResource(R.drawable.leo);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("LEO \n\nLeo es un signo de fuego regido por el sol, naturalmente son personas sociables y extrovertidas. Disfrutan de relacionarse con mucha gente y de tener muchos amigos. Les gusta llamar la atención, son carismáticos y orgullosos. \n" +
+                         txtData.setText(name + " tu signo es LEO \n\nLeo es un signo de fuego regido por el sol, naturalmente son personas sociables y extrovertidas. Disfrutan de relacionarse con mucha gente y de tener muchos amigos. Les gusta llamar la atención, son carismáticos y orgullosos. \n" +
                                  "\n" +
                                  "Normalmente se cree que personas de Leo son soberbias, pero en realidad no lo son. Lo que ocurre es que, por lo general, Leo siente que no tiene nada que ocultar y si tiene algo positivo para enseñar a los demás, lo hará sin problemas. ");
-                     }else if(dayBirth >= 23 && dayBirth <= 31 && monthBirth == 8 || dayBirth <= 22 && dayBirth >= 1 && monthBirth == 9)
+                     }else if(day >= 23 && day <= 31 && month == 8 || day <= 22 && day >= 1 && month == 9)
                      {
                          image.setImageResource(R.drawable.virgo);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("VIRGO \n\nVirgo es un signo de tierra, de naturaleza práctica, con un estilo metódico que le permite alcanzar sus objetivos. Están regidos por Mercurio y suelen ser muy claros en lo que quieren. \n" +
+                         txtData.setText(name + " tu signo es VIRGO \n\nVirgo es un signo de tierra, de naturaleza práctica, con un estilo metódico que le permite alcanzar sus objetivos. Están regidos por Mercurio y suelen ser muy claros en lo que quieren. \n" +
                                  "\n" +
                                  "En el ámbito laboral suelen ser muy eficientes y llegan a destacarse por dicha capacidad. Son capaces de analizar diversas situaciones y llegar a una conclusión acertada. ");
 
-                     }else if(dayBirth >= 23 && dayBirth <= 30 && monthBirth == 9 || dayBirth <= 22 && dayBirth >= 1 && monthBirth == 10)
+                     }else if(day >= 23 && day <= 30 && month == 9 || day <= 22 && day >= 1 && month == 10)
                      {
                          image.setImageResource(R.drawable.libra);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("LIBRA \n\nLibra es un signo de aire regido por Venus. Los hombres y mujeres de este signo poseen una naturaleza extrovertida, son comunicativos y sociables. Buscan constantemente el equilibrio y se sienten muy atraídos por los desafíos intelectuales. \n" +
+                         txtData.setText(name + " tu signo es LIBRA \n\nLibra es un signo de aire regido por Venus. Los hombres y mujeres de este signo poseen una naturaleza extrovertida, son comunicativos y sociables. Buscan constantemente el equilibrio y se sienten muy atraídos por los desafíos intelectuales. \n" +
                                  "\n" +
                                  "En el ámbito del amor, el hombre de Libra suele tener mucha elocuencia y la utiliza como arma de seducción. Son hombres con mucho encanto para comunicarse y poseen un gran dominio del lenguaje. ");
-                     }else if(dayBirth >= 23 && dayBirth <= 31 && monthBirth == 10 || dayBirth <= 21 && dayBirth >= 1 && monthBirth == 11)
+                     }else if(day >= 23 && day <= 31 && month == 10 || day <= 21 && day >= 1 && month == 11)
                      {
                          image.setImageResource(R.drawable.scorpio);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("ESCORPIÓN  \n\nEscorpio es un signo regido por Plutón, planeta de la intensidad y de las pasiones. \n" +
+                         txtData.setText(name + " tu signo es ESCORPIÓN  \n\nEscorpio es un signo regido por Plutón, planeta de la intensidad y de las pasiones. \n" +
                                  "\n" +
                                  "Los hombres y mujeres de este signo tienen una personalidad absorbente y quieren abarcarlo todo. Son tenaces y muy exigentes sí mismos y también con los demás. Sin embargo, nunca exigirá a los demás, más de lo que se exige a sí mismo. ");
-                     }else if(dayBirth >= 22 && dayBirth <= 30 && monthBirth == 11 || dayBirth <= 21 && dayBirth >= 1 && monthBirth == 12)
+                     }else if(day >= 22 && day <= 30 && month == 11 || day <= 21 && day >= 1 && month == 12)
                      {
                          image.setImageResource(R.drawable.sagitario);
                          image.setVisibility(View.VISIBLE);
                          txtData.setVisibility(View.VISIBLE);
-                         txtData.setText("SAGITARIO  \n\nEs un signo regido por Júpiter, planeta que se relaciona con la abundancia y la alegría. \n" +
+                         txtData.setText(name + " tu signo es SAGITARIO  \n\nEs un signo regido por Júpiter, planeta que se relaciona con la abundancia y la alegría. \n" +
                                  "\n" +
                                  "Sagitario es un signo de fuego, de naturaleza expansiva y alegre. Muy optimista y confiado en las posibilidades que pueda brindarle la vida. Por este motivo, en ocasiones suele plantearse metas exageradas. ");
                      }else
